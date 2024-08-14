a212d1f360a6ceb19e5066a7eae121599044ebce
Elnur Isgandarov <elnur1isgenderov@gmail.com>
2024-08-14 18:23:06 +0200

    Update: finding next_prime number and repeat the operation of calculation

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from calculate import add_numbers, subtract_numbers, multiply_numbers, divide_numbers
+from utils import next_prime
 
 app = FastAPI()
 
@@ -17,15 +18,22 @@ async def calculate(payload: CalculatePayload):
     b = payload.b
     operation = payload.operation
 
+    higher_num = max(a, b)
+    prime = next_prime(higher_num)
+
     if operation == "add":
         result = add_numbers(a, b)
+        result = add_numbers(result, prime)
     elif operation == "subtract":
         result = subtract_numbers(a, b)
+        result = subtract_numbers(result, prime)
     elif operation == "multiply":
         result = multiply_numbers(a, b)
+        result = multiply_numbers(result, prime)
     elif operation == "divide":
         try:
             result = divide_numbers(a, b)
+            result = divide_numbers(result, prime)
         except ValueError as e:
             raise HTTPException(status_code=400, detail=str(e))
     else:
